991783d7a4244abbddbcf159cbf768411bf2f843
Hackbright Student <info@hackbrightacademy.com>
2016-07-29 12:51:42 -0700

    added melon order function

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -37,11 +37,30 @@ $("#weather-form").on('submit', showWeather);
 
 // PART 3: ORDER MELONS
 
+
+function confirmOrder(results) {
+    if (results.code === 'ERROR') {
+        $('#order-status').addClass('order-error');
+    } else {
+        $('#order-status').removeClass('order-error');
+    }
+
+    $('#order-status').html("Status: " + results.code + "<br>" + results.msg);
+
+}
+
+
 function orderMelons(evt) {
     evt.preventDefault();
 
     // TODO: show the result message after your form
     // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+    var formInputs = {
+        "qty": $("#qty-field").val(),
+        "melon_type": $("#melon-type-field").val()
+    };
+
+    $.post('/order-melons.json', formInputs, confirmOrder)
 }
 
 $("#order-form").on('submit', orderMelons);
